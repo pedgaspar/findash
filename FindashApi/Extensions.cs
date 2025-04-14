b60b360b760944141a717e0af5ade709ee84848d
Pedro Gaspar <pedro.gaspar@setwin.pt>
2025-04-14 18:16:40 +0100

    First API Controller and endpoints

---
+++ FindashApi/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Findash;
+
+public static class Extensions
+{
+    public static ModelStateDictionary ToModelStateDictionary(this ValidationResult validationResult)
+    {
+        var modelState = new ModelStateDictionary();
+
+        foreach (var error in validationResult.Errors)
+        {
+            modelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+
+        return modelState;
+    }
+}
--- FindashApi/Program.cs
+++ FindashApi/Program.cs
@@ -1,8 +1,18 @@
+using Findash;
+using Findash.Abstractions;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IRepository<Employee>, EmployeeRepository>();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddProblemDetails();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,11 +24,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapControllers();
 
+<<<<<<< Updated upstream:Findash/Program.cs
 app.MapGet("/weatherforecast", () =>
 {
         var forecast = Enumerable.Range(1, 5).Select(index =>
@@ -39,3 +47,6 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+=======
+app.Run();
+>>>>>>> Stashed changes:FindashApi/Program.cs
